6c372e2f440b3cc5f215b695f94f25e61ecb8a6a
garos4 <benmba4@gmail.com>
2026-09-10 12:44:38 +0000

    Update script

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,44 +1,10 @@
-@if (
-    Astrogoat\ConvertExperience\Settings\ConvertExperienceSettings::isEnabled() &&
-    ($url = settings('Astrogoat\\ConvertExperience\\Settings\\ConvertExperienceSettings', 'url'))
-)
-    <!-- [ConvertExperience] Start -->
-    <script>
-        (function() {
-            var s = document.createElement('script');
-            var h = document.querySelector('head') || document.body;
-            s.src = "{{ $url }}";
-            s.async = true;
-            s.onload = function() {
-                acsbJS.init({
-                    statementLink: '',
-                    footerHtml: '',
-                    hideMobile: false,
-                    hideTrigger: false,
-                    disableBgProcess: false,
-                    language: 'en',
-                    position: 'left',
-                    leadColor: '#000000',
-                    triggerColor: '#000000',
-                    triggerRadius: '50%',
-                    triggerPositionX: 'left',
-                    triggerPositionY: 'bottom',
-                    triggerIcon: 'people',
-                    triggerSize: 'small',
-                    triggerOffsetX: 20,
-                    triggerOffsetY: 20,
-                    mobile: {
-                        triggerSize: 'small',
-                        triggerPositionX: 'left',
-                        triggerPositionY: 'bottom',
-                        triggerOffsetX: 10,
-                        triggerOffsetY: 10,
-                        triggerRadius: '50%'
-                    }
-                });
-            }
-            h.appendChild(s);
-        })();
-    </script>
-    <!-- [ConvertExperience] End -->
+@php
+    use Astrogoat\ConvertExperience\Settings\ConvertExperienceSettings;
+    $settings = resolve(ConvertExperienceSettings::class);
+@endphp
+
+@if($settings->enabled)
+<!-- begin Convert Experiences code-->
+    <script type="text/javascript" src="{{ $settings->url }}"></script>
+<!-- end Convert Experiences code -->
 @endif
