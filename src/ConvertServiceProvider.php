0fcd89978f1c473bda2d1c05bb5912a2f6c01a6e
garos4 <garos4@users.noreply.github.com>
2026-09-11 15:00:35 +0000

    Fix styling

--- src/ConvertServiceProvider.php
+++ src/ConvertServiceProvider.php
@@ -19,7 +19,7 @@ class ConvertServiceProvider extends PackageServiceProvider
             ->migrations([
                 __DIR__ . '/../database/migrations/settings',
             ])->includeFrontendViews(function (IncludeFrontendViews $views) {
-                return $views->addToHead(['convert::script'],101);
+                return $views->addToHead(['convert::script'], 101);
             });
     }
 
